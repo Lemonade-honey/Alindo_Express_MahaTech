947d56a2e8c1bc58bb4b4dad9c97f82907dc7ea5
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-05-29 20:34:10 +0700

    penggantian nama atribut di database riwayat_paket -> update_paket

--- app/Domain/Paket.php
+++ app/Domain/Paket.php
@@ -8,15 +8,15 @@
 
 namespace Mahatech\AlindoExpress\Domain;
 class Paket{
-    public ?int $kodeResi = null;
+    public ?string $kodeResi = null;
     public ?string $tanggalPembuatan = null;
     public ?string $dataPaket = null;
     public ?string $biayaPaket = null;
     public ?string $vendorPaket = null;
-    public ?string $riwayatPaket = null;
+    public ?string $updatePaket = null;
 
     // Fitur Track Paket belum tersedia
     // public ?array $trackPaket = null;
-    public ?int $statusPaket = null;
+    public ?string $statusPaket = null;
 
 }
