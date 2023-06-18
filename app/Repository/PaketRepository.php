3e0e20c5a2d969af6e7d897a15da67d106878713
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-18 14:22:15 +0700

    Penambahan get data berddasarkan tanggal dan beberapa komentar

--- app/Repository/PaketRepository.php
+++ app/Repository/PaketRepository.php
@@ -17,6 +17,7 @@ class PaketRepository{
      * Save data ke paket tabel
      * 
      * data yang disimpan ke atribut : resi, tanggal pembuatan, data paket, biaya paket, vendor paket, update paket.
+     * @return Paket
      */
     public function save(Paket $paket): Paket{
         $stmt = $this->connection->prepare('INSERT INTO paket(resi, tanggal_pembuatan, data_paket, biaya_paket, vendor_paket, update_paket, status_paket) VALUES(?, ?, ?, ?, ?, ?, ?)');
@@ -29,9 +30,10 @@ class PaketRepository{
      * Show All Data Paket
      * 
      * Menampilkan semua isi data dari database
+     * @return array | null
      */
     public function getAllData(): ?array{
-        $stmt = $this->connection->prepare('SELECT resi, tanggal_pembuatan, data_paket, status_paket FROM paket');
+        $stmt = $this->connection->prepare('SELECT resi, tanggal_pembuatan, data_paket, biaya_paket, vendor_paket, status_paket FROM paket');
         $stmt->execute();
 
         try {
@@ -48,6 +50,12 @@ class PaketRepository{
         }
     }
 
+    /**
+     * Find Data By Kode Resi
+     * 
+     * mencari data di database berdasarkan kode resi, dan return valuenya OBJ Paket jika data ditemukan
+     * @return Paket | null
+     */
     public function findByKodeResi(string $kodeResi): ?Paket{
         $stmt = $this->connection->prepare('SELECT resi, tanggal_pembuatan, data_paket, biaya_paket, vendor_paket, update_paket, status_paket FROM paket WHERE resi = ?');
         $stmt->execute([$kodeResi]);
@@ -73,6 +81,12 @@ class PaketRepository{
         return null;
     }
 
+    /**
+     * Check Kode Resi
+     * 
+     * check kode resi di db, sudah ada atau belum
+     * @return bool
+     */
     public function checkKodeResiInDatabase(string $kodeResi): bool{
         $stmt = $this->connection->prepare('SELECT resi FROM paket WHERE resi = ?');
         $stmt->execute([$kodeResi]);
@@ -90,6 +104,11 @@ class PaketRepository{
         }
     }
 
+    /**
+     * Get Last Kode Resi
+     * 
+     * mendapatkan kode resi yang terakhir atau yang paling baru di DB
+     */
     public function getLastKodeResi(): ?string{
         $stmt = $this->connection->prepare('SELECT resi FROM paket ORDER BY resi DESC LIMIT 1');
         $stmt->execute();
@@ -106,10 +125,42 @@ class PaketRepository{
         }
     }
 
+    /**
+     * Update Vendor
+     * 
+     * Update data vendor, hal yang diupdate vendor paket dan update paket (history)
+     */
     public function updateInvoiceVendor(Paket $paket): Paket{
         $stmt = $this->connection->prepare('UPDATE paket SET vendor_paket = ?, update_paket = ? WHERE resi = ? ');
         $stmt->execute([$paket->vendorPaket, $paket->updatePaket, $paket->kodeResi]);
 
         return $paket;
+    }
+
+    /**
+     * Get Data berdasarkan tanggal
+     * 
+     * mendapatkan data paket di DB berdasarkan tanggal yang dimasukkan
+     */
+    public function getDataByDate(string $tanggal): ?array{
+        $bulan = date('m', strtotime($tanggal));
+        $tahun = date('Y', strtotime($tanggal));
+        $stmt = $this->connection->prepare('SELECT * FROM paket WHERE YEAR(tanggal_pembuatan) = ? AND MONTH(tanggal_pembuatan) = ?');
+        $stmt->execute([$tahun, $bulan]);
+
+        try {
+            if($row = $stmt->fetchAll(PDO::FETCH_ASSOC)){
+                $array = [];
+                foreach($row as $data){
+                    $array[] = $data;
+                }
+
+                return $array;
+            }else{
+                return null;
+            }
+        } finally{
+            $stmt->closeCursor();
+        }
     }
 }
