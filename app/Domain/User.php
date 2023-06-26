03809c760b9a22bf86993a876f06ffa6a9ffea0a
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-26 11:31:32 +0700

    Penambahan Domain User dan User Repository

---
+++ app/Domain/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace Mahatech\AlindoExpress\Domain;
+class User{
+    public string $userId;
+    public string $userPassword;
+    public string $userData;
+    public int $userAccsessLevel;
+}
---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Mahatech\AlindoExpress\Repository;
+use Mahatech\AlindoExpress\Domain\User;
+use PDO;
+class UserRepository{
+    private PDO $connection;
+    public function __construct(PDO $connection) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Save User Data
+     */
+    public function save(User $user): User{
+        $stmt = $this->connection->prepare('INSERT INTO users(id, password, data, access_level) VALUES(?, ?, ?, ?)');
+        $stmt->execute([$user->userId, $user->userPassword, $user->userData, $user->userAccsessLevel]);
+
+        return $user;
+    }
+
+    /**
+     * Search User By Id
+     */
+    public function findById(string $userId): ?User{
+        $stmt = $this->connection->prepare('SELECT * FROM users WHERE id = ?');
+        $stmt->execute([$userId]);
+
+        try{
+            if($row = $stmt->fetch()){
+                $user = new User;
+                $user->userId = $row['id'];
+                $user->userPassword = $row['password'];
+                $user->userData = $row['data'];
+                $user->userAccsessLevel = $row['access_level'];
+
+                return $user;
+            }else{
+                return null;
+            }
+        } finally{
+            $stmt->closeCursor();
+        }
+    }
+}
