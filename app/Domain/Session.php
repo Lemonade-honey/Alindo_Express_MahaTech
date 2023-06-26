5584463a050ab274b38727713c98aea43695adc7
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-26 10:37:21 +0700

    Penambahan session domain

---
+++ app/Domain/Session.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mahatech\AlindoExpress\Domain;
+class Session{
+    public string $idSession;
+    public string $userId;
+    public int $accessLevel;
+    public string $tanggal;
+}
